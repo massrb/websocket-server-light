68b8f5c9f4902a846ec5fccb92dc5970649ab045
laurence <massrubydev@gmail.com>
2025-07-30 19:03:46 -0400

    recommended changes

--- app.rb
+++ app.rb
@@ -2,46 +2,55 @@ require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'faye/websocket'
 require 'json'
-require 'thread'
 
 class App < Sinatra::Base
-  set :database, { adapter: "sqlite3", database: "db/development.sqlite3" }
-
+  set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
+  
   @connections = []
-
-  get '/' do
-    erb :index
-  end
 
   def self.connections
     @connections
   end
 
-  # Override instance method call
+  get '/' do
+    erb :index
+  end
+
   def call(env)
     if Faye::WebSocket.websocket?(env) && env['PATH_INFO'] == '/websocket'
       puts "Incoming request: #{env['REQUEST_METHOD']} #{env['PATH_INFO']}"
-      ws = Faye::WebSocket.new(env)
+      ws = Faye::WebSocket.new(env, nil, ping: 15)
 
       ws.on :open do |_|
         self.class.connections << ws
-        puts 'WebSocket opened'
+        puts "WebSocket opened"
       end
 
       ws.on :message do |event|
-        data = JSON.parse(event.data)
-        msg = Message.create(content: data['content'])
-        self.class.connections.each { |conn| conn.send({ id: msg.id, content: msg.content }.to_json) }
+        begin
+          data = JSON.parse(event.data)
+          msg = Message.create(content: data['content'])
+          self.class.connections.each do |conn|
+            conn.send({ id: msg.id, content: msg.content }.to_json)
+          end
+        rescue => e
+          puts "Error in message handler: #{e.message}"
+        end
       end
 
-      ws.on :close do |_|
+      ws.on :close do |event|
         self.class.connections.delete(ws)
-        puts 'WebSocket closed'
+        puts "WebSocket closed, code=#{event.code}, reason=#{event.reason}"
+        ws = nil
       end
 
-      ws.rack_response
+      ws.on :error do |event|
+        puts "WebSocket error: #{event.message}"
+      end
+
+      return ws.rack_response
     else
-      super(env)  # call Sinatra's normal handler
+      super(env)
     end
   end
 end
